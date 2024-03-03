d37b82ce2c9f85e02c97e308f3739bdf460eaeb4
bacho1927 <bacho.karashvili@gmail.com>
2024-03-03 19:02:34 +0400

    Update Main.jsx

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -2,7 +2,7 @@
 import { useAppContext } from './ContextProvider';
 import ImageModal from './ImageModal';
 import FetchData from './FetchData';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 
